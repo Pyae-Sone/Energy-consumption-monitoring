c2faba78f17a9f848e1196fa067555b3b4c2bdb0
Pyae Sone San <52834048+Pyae-Sone@users.noreply.github.com>
2023-08-24 22:40:04 +0630

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,9 +1,10 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <ArduinoJson.h>
 
-const char* apSSID = "";
-const char* apPassword = "";
-const IPAddress apIP(192, 168, 10, 1); // Change this to your desired IP address
+const char* apSSID = "MyESP8266AP";
+const char* apPassword = "1234";
+const IPAddress apIP(192, 168, 10, 1);
 
 const int currentPin = A0;
 
@@ -14,95 +15,47 @@ void setup() {
 
   pinMode(currentPin, INPUT);
 
-
-  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0)); // Specify subnet mask
-  // Set the ESP8266 as an Access Point
+  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
   WiFi.softAP(apSSID, apPassword);
 
   Serial.print("AP IP address: ");
   Serial.println(WiFi.softAPIP());
 
-  server.on("/", HTTP_GET, handleRoot);
+  // Enable CORS headers
+  server.onNotFound([]() {
+    if (server.method() == HTTP_OPTIONS) {
+      server.sendHeader("Access-Control-Allow-Origin", "*");
+      server.sendHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+      server.sendHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      server.send(204);
+    } else {
+      server.send(404, "text/plain", "Not Found");
+    }
+  });
+
+  server.on("/api", HTTP_GET, [](){
+    float electricCurrent = analogRead(currentPin) * 0.0049;
+
+    // Create a JSON object to hold the electric current value
+    StaticJsonDocument<64> jsonDocument;
+    jsonDocument["current"] = electricCurrent;
+
+    // Serialize the JSON to a string
+    String jsonString;
+    serializeJson(jsonDocument, jsonString);
+
+    // Set CORS headers for the API response
+    server.sendHeader("Access-Control-Allow-Origin", "*");
+    server.send(200, "application/json", jsonString);
+  });
+
+  server.on("/", HTTP_GET, [](){
+    server.send(200, "text/html", "<h1>ESP8266 API</h1><p>Visit /api to get electric current data.</p>");
+  });
 
   server.begin();
 }
 
 void loop() {
   server.handleClient();
-}
-
-void handleRoot() {
-  float electricCurrent = analogRead(currentPin) * 0.0049; // Convert ADC reading to current (adjust as needed)
-
-  String html = R"html(
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Electricity Current Monitor</title>
-  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-</head>
-<body>
-  <div class="container">
-    <h1>Electricity Current Monitor</h1>
-    <p>Current: <span id="currentValue">0 A</span></p>
-    <canvas id="currentGraph" width="400" height="200"></canvas>
-  </div>
-  <script>
-    const currentValueElement = document.getElementById('currentValue');
-    const currentGraph = new Chart(document.getElementById('currentGraph').getContext('2d'), {
-      type: 'line',
-      data: {
-        labels: [],
-        datasets: [{
-          label: 'Current (A)',
-          data: [],
-          borderColor: 'blue',
-          backgroundColor: 'rgba(0, 0, 255, 0.1)',
-          fill: true
-        }]
-      },
-      options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-          x: {
-            type: 'linear',
-            position: 'bottom'
-          },
-          y: {
-            min: 0,
-            max: 10 // Adjust as needed
-          }
-        }
-      }
-    });
-
-    // Update the current value and graph with the C variable's value
-    function updateData(current) {
-      currentValueElement.textContent = current + ' A';
-      currentGraph.data.labels.push('');
-      currentGraph.data.datasets[0].data.push(current);
-      currentGraph.update();
-    }
-
-    // Initialize with the C variable's value
-    updateData(parseFloat(`)html" + String(electricCurrent) + R"html(`));
-
-    // Simulate receiving data and updating the UI
-    function simulateDataUpdate() {
-      const randomCurrent = Math.random() * 10; // Random value between 0 and 10
-      updateData(randomCurrent);
-      setTimeout(simulateDataUpdate, 1000); // Update every 1 second
-    }
-
-    // Start simulating data updates
-    simulateDataUpdate();
-  </script>
-</body>
-</html>
-)html";
-
-  server.send(200, "text/html", html);
 }
